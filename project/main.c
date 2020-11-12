280af79f4bd1cc4a276209b10ce7f2685ccda088
freddii92 <arodriguez242@miners.utep.edu>
2020-11-12 15:29:29 -0700

    added house going from right to left of screen

--- project/main.c
+++ project/main.c
@@ -7,17 +7,17 @@
 #define LED_GREEN BIT6        // P1.6
 
 short redrawScreen = 1;
-u_int row = 10;
-u_int col = 10;
+u_int row = screenHeight/2;
+u_int col = screenWidth;
 
 void wdt_c_handler()
 {
   static int count = 0;
 
   count++;
-  if (count == 150) {
+  if (count == 20) {
     count = 0;
-    row++;
+    col--;
     redrawScreen = 1;
   }
 }
@@ -38,11 +38,23 @@ void main()
   while (1) {
     if (redrawScreen) {
       redrawScreen = 0;
-      clearScreen(COLOR_WHITE);
-      if (row == screenHeight) {
-	row = 0;
+      if (col == 0) {
+	col = screenWidth;
+	clearScreen(COLOR_WHITE);
       }
-      drawString5x7(col, row, "TEST", COLOR_BLACK, COLOR_WHITE);
+      for (int r = 0; r < 10; r++) {
+	for (int c = 0; c < r; c++) {
+	  drawPixel(col + c + 7, r + row - 10, COLOR_BLACK);
+	  drawPixel(col - c + 7, r + row - 10, COLOR_BLACK);
+	  drawPixel(col + c + 8, r + row - 10, COLOR_WHITE);
+	}
+      }
+      for (int i = 0; i < 15; i++) {
+	for (int j = 0; j < 15; j++) {
+	  drawPixel(i + col, j + row, COLOR_BLACK);
+	  drawPixel(i + col + 15, j + row, COLOR_WHITE);
+	}
+      }
     }
     P1OUT &= ~LED_GREEN;      // green off
     or_sr(0x10);              // CPU off
